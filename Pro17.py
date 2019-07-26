15a23be684e602e056ed4398f8d11fdf39a5df5e
Srinithi06 <52853027+Srinithi06@users.noreply.github.com>
2019-07-26 21:30:56 +0530

    Create Pro17.py

---
+++ Pro17.py
@@ -0,0 +1,9 @@
+ccom,ddom=map(int,input().split())
+l=list(map(int,input().split()))
+b=0
+for i in range(0,ccom):
+    for j in range(2,ccom):
+        if l[i]+[j]==ddom and i!=j
+            b=1
+            break
+ print('yes' if b else 'no')
