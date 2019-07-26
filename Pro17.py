4cec2b74f57ca03e11cbb9d49e82261ed47d331e
Srinithi06 <52853027+Srinithi06@users.noreply.github.com>
2019-07-26 22:02:48 +0530

    Update Pro17.py

--- Pro17.py
+++ Pro17.py
@@ -3,7 +3,7 @@ l=list(map(int,input().split()))
 b=0
 for i in range(0,ccom):
     for j in range(1,ccom):
-        if l[i]+[j]==ddom and i!=j:
+        if l[i]+l[j]==ddom and i!=j:
             b=1
             break
  print("yes" if b else "no")
