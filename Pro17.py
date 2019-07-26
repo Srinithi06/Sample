85acecfc659cefbee76b0a7c2f4dd4f6966166ab
Srinithi06 <52853027+Srinithi06@users.noreply.github.com>
2019-07-26 21:36:13 +0530

    Update Pro17.py

--- Pro17.py
+++ Pro17.py
@@ -2,7 +2,7 @@ ccom,ddom=map(int,input().split())
 l=list(map(int,input().split()))
 b=0
 for i in range(0,ccom):
-    for j in range(2,ccom):
+    for j in range(1,ccom):
         if l[i]+[j]==ddom and i!=j:
             b=1
             break
